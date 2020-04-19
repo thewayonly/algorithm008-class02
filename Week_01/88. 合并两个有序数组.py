519ce7562ca9b29af30c8c2e5b2d7bb33605f6f2
hopezh <30565337+thewayonly@users.noreply.github.com>
2020-04-20 00:38:04 +0800

    Create 88. 合并两个有序数组.py

---
+++ Week_01/88. 合并两个有序数组.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        # pointers for nums1 and nums2, start from ending
+        p1 = m - 1
+        p2 = n - 1
+        # set pointer for final nums1
+        p = m + n - 1
+        
+        # while there are still elements to compare
+        while p1 >= 0 and p2 >= 0:
+            if nums1[p1] < nums2[p2]:
+                nums1[p] = nums2[p2]
+                p2 -= 1
+            else:
+                nums1[p] =  nums1[p1]
+                p1 -= 1
+            p -= 1
+        
+        nums1[:p2 + 1] = nums2[:p2 + 1]
