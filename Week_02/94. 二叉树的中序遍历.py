48bdbccbcaccebddb7ec46cd3bbecd7550b23d82
hopezh <30565337+thewayonly@users.noreply.github.com>
2020-04-27 00:15:19 +0800

    Create 94. 二叉树的中序遍历.py

---
+++ Week_02/94. 二叉树的中序遍历.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        stack,rst = [root],[]
+        while stack:
+            i = stack.pop()
+            if isinstance(i,TreeNode):
+                stack.extend([i.right,i.val,i.left])
+            elif isinstance(i,int):
+                rst.append(i)
+        return rst
